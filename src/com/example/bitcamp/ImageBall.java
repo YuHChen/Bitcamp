9dcc5cc2cb759f7978ded92cb9fc6533fb907265
YuHChen <yuhchen21@gmail.com>
2014-04-05 04:23:08 -0400

    Added getter and setter for private mBall.

--- src/com/example/bitcamp/ImageBall.java
+++ src/com/example/bitcamp/ImageBall.java
@@ -4,16 +4,30 @@ import android.angle.AnglePhysicObject;
 import android.angle.AngleCircleCollider;
 
 public class ImageBall extends ImageView {
-    
+
+    private AnglePhysicObject mBall;
+
     public ImageBall(Context context, float x, float y){
 	super(context);
-	AnglePhysicObject ball = new AnglePhysicObject(0,1);
-	ball.mPosition = new AngleVector(x,y);
+	mBall = new AnglePhysicObject(0,1);
+	mBall.mPosition = new AngleVector(x,y);
 	AngleCircleCollider ballCollider = new AngleCircleCollider(x,y,5f);
-	ball.addCircleCollider(ballCollider);
+	mBall.addCircleCollider(ballCollider);
+	mBall.mMass = 1;
+	mBall.mVelocity = new AngleVector(0,1f);
+	mBall.mDelta = new AngleVector(1f,1f);
     }
 
     public ImageBall(Context context){
 	this(context,0,0);
     }
+
+    public AnglePhysicObject getBall(){ 
+	return mBall; 
+    }
+
+    public int setBall(AnglePhysicObject ball){ 
+	mBall = ball; 
+    }
+
 }
